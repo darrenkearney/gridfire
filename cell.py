fcba509e2daed169b7260bc180e2ad7bd2e7fb79
Darren Kearney <github@darrenk.net>
2016-04-10 14:43:56 +0100

    arrays of classes confusion sorted

--- cell.py
+++ cell.py
@@ -1,6 +1,8 @@
 class Cell ():
     def __init__(self, **kwargs):
-        self.coord = kwargs.get('coord', [0,0])
+        self.x = kwargs.get( 'x', 0 )
+        self.y = kwargs.get( 'y', 0 )
+        self.coord = kwargs.get('coord', [ self.x, self.y ])
         self.visited = kwargs.get('visited', 'false')
         for key, value in kwargs.items():
             setattr(self, key, value)
--- level.py
+++ level.py
@@ -25,3 +25,37 @@ for y in range(3):
     #for cell in cell_list:
     #   levelx.append(cell)
     #cell_list[0].coord
+
+print(level[1][1]   )
+print('This is the cell coordinates from the cell at 1,1')
+print("Level Cell: {}".format(level[1][1]))
+
+
+
+# micheal tip 
+# for index, item in enumerate([3,4,5]):
+
+outer_array = []
+for y in range(3):
+     #print(y)
+     #print(outer_array)
+     inner_array = []
+     for x in range(3):
+             #print(x)
+             #print(inner_array)
+             cell = Cell(x=x, y=y)
+             #obj = {'coord': [x, y]}
+             #inner_array.append([]) #append empty array, this will be how we select the object
+             inner_array.append(cell)
+     outer_array.append(inner_array)
+     print(outer_array)
+
+print('Cell 0,0')
+print( outer_array[0][1].x )
+
+print( "Cell['coord'] = {}".format(outer_array[0][0].coord) )
+# test =  outer_array[0][0] 
+# for _ in test:
+#     item.coord
+# print('This is the cell coordinates from the cell at 1,1')
+# print("Cell: {}".format(outer_array[1][1]))
